a54302c99f291605299a831b051b62893fcdf74d
imenmansouri93 <imennmansourii@gmail.com>
2022-09-02 22:37:02 +0100

    firstcommit

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * @brief
+ * 
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if (tree->left == NULL && tree->right == NULL)
+{
+return (0);
+}
+return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
