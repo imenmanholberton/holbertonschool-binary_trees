4d21bbb195d6f8858eeae8d4d3d5fce4956553b6
imenmansouri93 <imennmansourii@gmail.com>
2022-09-02 15:47:17 +0100

    unction that goes through a binary tree using in-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 /**
- * @brief
- * 
+ * binary_tree_inorder - function that goes through a binary tree using
+ * in-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func:  pointer to a function to call for each node
+ * Return: Nothing
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
