412db479d6e25da50849e40b1dd6c75878ecb072
imenmansouri93 <imennmansourii@gmail.com>
2022-09-02 07:03:24 +0100

    newnode

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,21 +4,30 @@
  *
  *
  */
-binary_tree_t *newNode(int value)
-{
-binary_tree_t *res;
-res =(binary_tree_t  *) malloc(sizeof(binary_tree_t));
-res->n= value;
-res->left = res->right = NULL;
-return res;
-}
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+binary_tree_t *newNode;
+binary_tree_t *aux;
 if (parent == NULL)
-return newNode(value);
-if(value < parent->n)
-parent->left = binary_tree_insert_left(parent->left, value);
+return (NULL);
+newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+if(newNode == NULL)
+return (NULL);
+newNode->n = value;
+newNode->left = NULL;
+newNode->right = NULL;
+if (parent->left == NULL)
+{
+parent->left = newNode;
+newNode->parent = parent;
+}
 else
-parent->right = binary_tree_insert_left(parent->right, value);
-return parent;
+{
+aux = parent->left;
+parent->left = newNode;
+newNode->left = aux;
+newNode->parent = parent;
+aux->parent = newNode;
+}
+return (newNode);
 }
