fcc3eb7edac0c4f2b08deadf41088d4e273a4102
imenmansouri93 <imennmansourii@gmail.com>
2022-09-03 11:13:50 +0100

    depthtree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,15 +6,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-size_t a, b;
-if (tree == NULL)
+if (tree == NULL ||  tree->parent == NULL)
 {
 return (0);
 }
-a =  binary_tree_depth(tree->left);
-b =  binary_tree_depth(tree->right);
-if(a > b)
-return (a + 1);
-else
-return (b + 1);
+
+return (binary_tree_depth(tree->parent) + 1);
 }
